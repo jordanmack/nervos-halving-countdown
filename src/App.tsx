45409b2b2e62b5d44d0caaad367c3009d53de391
Jordan Mack <jordan.mack@brilliantnotion.com>
2023-05-14 13:52:17 -0700

    Update layout, graphics, and text.

--- src/App.tsx
+++ src/App.tsx
@@ -6,51 +6,83 @@ import './App.scss';
 /**
  * Constants
  */
-const CKB_RPC_URL = 'https://mainnet.ckb.dev/rpc';	// The JSON RPC URL of the CKB Full Node to query.
-const EPOCHS_PER_HALVING = 8760;					// The number of epochs per halving. This should never change.
-const HOURS_PER_EPOCH = 4;							// The number of hours per epoch. This should never change.
-const TICK_DELAY = 500;								// The delay in milliseconds to update the countdown.
-const REFRESH_DELAY = 11 * 1000;					// The delay in milliseconds to refresh the RPC data and update the current block and epoch.
-const FULL_REFRESH_DELAY = 5 * 60 * 1000;			// The delay in milliseconds to refresh all RPC data and update all current values and target values.
+const CKB_RPC_URL = 'https://rpc.ankr.com/nervos_ckb';	// The JSON RPC URL of the CKB Full Node to query.
+const EPOCHS_PER_HALVING = 8760;						// The number of epochs per halving. This should never change.
+const HOURS_PER_EPOCH = 4;								// The number of hours per epoch. This should never change.
+const TICK_DELAY = 500;									// The delay in milliseconds to update the countdown.
+const REFRESH_DELAY = 11 * 1000;						// The delay in milliseconds to refresh the RPC data and update the current block and epoch.
+const FULL_REFRESH_DELAY = 5 * 60 * 1000;				// The delay in milliseconds to refresh all RPC data and update all current values and target values.
 
 /**
- * Generates a string that describes the amount of time remaining in the countdown.
+ * Generates time values in months, days, hours, minutes, and seconds based on the time remaining in the countdown.
  * @param timeFromNow The number of milliseconds until the target time.
- * @returns null
+ * @returns object
  */
-function calculateTimeString(timeFromNow: number)
+function calculateTimeValues(timeFromNow: number)
 {
-	// If the time has passed, display a message instead of a time.
-	if(timeFromNow < 1)
-		return 'Happy Halving! 🎈🎉🎈🍾';
-
 	// Time constants (in milliseconds).
 	const second = 1000;
 	const minute = second * 60;
 	const hour = minute * 60;
 	const day = hour * 24;
 	const month = day * 30;
-	const year = day * 365;
 
 	// Calculate values for the target.
-	const years = Math.floor(timeFromNow / year);
-	const months = Math.floor((timeFromNow - (years * year)) / month);
-	const days = Math.floor((timeFromNow - (months * month + years * year)) / day);
-	const hours = Math.floor((timeFromNow - (days * day + months * month + years * year)) / hour);
-	const minutes = Math.floor((timeFromNow - (hours * hour + days * day + months * month + years * year)) / minute);
-	const seconds = Math.floor((timeFromNow - (minutes * minute + hours * hour + days * day + months * month + years * year)) / second);
+	const months = Math.floor((timeFromNow) / month);
+	const days = Math.floor((timeFromNow - (months * month)) / day);
+	const hours = Math.floor((timeFromNow - (days * day + months * month)) / hour);
+	const minutes = Math.floor((timeFromNow - (hours * hour + days * day + months * month)) / minute);
+	const seconds = Math.floor((timeFromNow - (minutes * minute + hours * hour + days * day + months * month)) / second);
 
-	// Create the time string from the target values.
-	let string = '';
-	if(years > 0) { string += years+' year'+(years>1?'s':'')+', '; }
-	if(months > 0) { string += months+' month'+(months>1?'s':'')+', '; }
-	if(days > 0) { string += days+' day'+(days>1?'s':'')+', '; }
-	if(hours > 0) { string += hours+' hour'+(hours>1?'s':'')+', '; }
-	if(minutes > 0) { string += minutes+' minute'+(minutes>1?'s':'')+', '; }
-	if(minutes > 0) { string += ' and '+seconds+' second'+(seconds!==1?'s':'')+'. '; }
-	else { string += ' '+seconds+' second'+(seconds!==1?'s':'')+' '; }
+	// Create the time object from the target values.
+	let object = {months: 0, days: 0, hours: 0, minutes: 0, seconds: 0};
+	object.months = months;
+	object.days = days;
+	object.hours = hours;
+	object.minutes = minutes;
+	object.seconds = seconds;
 
-	return string;
+	return object;
+}
+
+/**
+ * Renders a counter circle with the specified information.
+ * @param value The number value for the middle of the circle.
+ * @param label The label to display below the circle.
+ * @returns JSX code to render a counter circle.
+ */
+function renderCounterCircle(value: number, label: string)
+{
+	const html =
+	(
+		<div className="time-value months inline-block relative w-[60px] md:w-[115px] mx-1 leading-none">
+			<img className="circle block w-full" src="countdown-circle.png" alt="Counter Circle" />
+			<span className="number absolute w-full top-4 md:top-9 left-0 text-center text-teal-300 text-xl md:text-4xl">{value}</span>
+			<label className="text-white text-base md:text-xl leading-">{label}</label>
+		</div>
+	);
+
+	return html;
+};
+
+/**
+ * Renders a stats box based on the specified information.
+ * @param label The primary label for the stats box.
+ * @param metric The primary metric value.
+ * @param metricSm The secondary metric value, which will be displayed in a small text size.
+ * @returns JSX code to render a stats box.
+ */
+function renderStatsBox(label: string, metric: string|null, metricSm: string|null)
+{
+	const html =
+	(
+		<div className="stats-box grow text-center w-full shrink-0 my-1 md:max-w-[150px] rounded p-3 bg-gradient-to-b from-teal-600/30 to-teal-900/10">
+			<p className="my-0 text-center text-sm">{label}</p>
+			<p className="my-0 text-center text-lg">{metric} <small className="text-xs">{metricSm}</small></p>
+		</div>
+	);
+
+	return html;
 }
 
 /**
@@ -68,7 +100,7 @@ function updateTargetString(setTargetString: React.Dispatch<React.SetStateAction
 	if(targetEpoch)
 	{
 		const date = (new Date(targetTime)).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"});
-		string = `The next halving is estimated to be reached on ${date}.`;
+		string = `The next halving is estimated to be reached on ${date.replaceAll(' ', ' ')}.`;
 	}
 
 	setTargetString(string);
@@ -79,14 +111,36 @@ function updateTargetString(setTargetString: React.Dispatch<React.SetStateAction
  * @param setCountdown A React function to set the countdown string.
  * @param targetTime The approximate time of the next halving.
  */
-function updateCountdown(setCountdown: React.Dispatch<React.SetStateAction<string>>, targetTime: number)
+function updateCountdown(setCountdown: React.Dispatch<React.SetStateAction<JSX.Element>>, targetTime: number)
 {
 	if(targetTime)
-		setCountdown(calculateTimeString(targetTime - Date.now()));
+	{
+		const timeRemaining = targetTime - Date.now();
+
+		// If there is still time remaining then display the countdown.
+		if(timeRemaining > 0)
+		{
+			const timeValues = calculateTimeValues(timeRemaining);
+			const html =
+			(
+				<div id="countdown" className="countdown text-2xl">
+					{renderCounterCircle(timeValues.months, "months")}
+					{renderCounterCircle(timeValues.days, "days")}
+					{renderCounterCircle(timeValues.hours, "hours")}
+					{renderCounterCircle(timeValues.minutes, "minutes")}
+					{renderCounterCircle(timeValues.seconds, "seconds")}
+				</div>
+			);
+			setCountdown(html);
+		}
+		// If the time has passed, display a message instead of a time.
+		else
+			setCountdown(<div id="countdown" className="text-4xl leading-[1.5em] md:text-6xl md:leading-[1.5em]">🎈Happy Halving!🎈<br />🎉🍾🎉🍾🎉</div>);
+	}
 }
 
 /**
- * Update all data using the CKB JSON RPC.
+ * Update all or specific data fields using the CKB JSON RPC.
  * @param setTargetTime A React function to set the target time of the next halving, or null to skip.
  * @param setTargetEpoch A React function to set the target epoch of the next halving, or null to skip.
  * @param setCurrentBlock A React function to set the current block number, or null to skip.
@@ -118,8 +172,7 @@ async function updateData(setTargetTime: React.Dispatch<React.SetStateAction<num
 		},
 		body: JSON.stringify(jsonRpcRequest)
 	}
-	let {result: {epoch: epochNumberWithFraction, number: blockNumber}} = await fetch(CKB_RPC_URL, fetchRequest)
-	.then(res => res.json());
+	let {result: {epoch: epochNumberWithFraction, number: blockNumber}} = await fetch(CKB_RPC_URL, fetchRequest).then(res => res.json());
 
 	// Decode block number. (RPC Documentation: https://github.com/nervosnetwork/ckb/blob/master/rpc/README.md#type-blocknumber)
 	blockNumber = Number(blockNumber);
@@ -154,7 +207,7 @@ function App()
 	const [currentEpochLength, setCurrentEpochLength] = useState(0);
 	const [targetTime, setTargetTime] = useState(0);
 	const [targetEpoch, setTargetEpoch] = useState(0);
-	const [countdown, setCountdown] = useState('...');
+	const [countdown, setCountdown] = useState(<div id="countdown" className="loading">&nbsp;</div>);
 	const [targetString, setTargetString] = useState(' ');
 
 	// Update the data from the RPC on load.
@@ -172,55 +225,51 @@ function App()
 
 	const html =
 	(
-		<div className="App">
-			<section className="max-w-screen-lg min-w-[375px] mx-auto px-2 md:px-0 py-24">
-				<p className="text-center text-2xl mb-2">
-					Nervos CKB Layer 1 Halving Countdown
-				</p>
-				<p className="text-center text-3xl mb-4">
+		<div className="App min-w-[375px]">
+			<section className="max-w-screen-md mx-auto px-1 md:px-0 pt-6 md:pt-24">
+				<h1 className="text-center mb-8 text-transparent font-bold bg-clip-text bg-gradient-to-r from-teal-300 to-teal-700">
+					Nervos&nbsp;CKB&nbsp;Layer&nbsp;1 Halving&nbsp;Countdown
+				</h1>
+				<h1 className="text-center mb-8">
 					{countdown}
-				</p>
-				<p className="text-center text-sm mb-12">
+				</h1>
+				<p className="text-center text-sm mb-12 text-teal-400">
 					{targetString}
 				</p>
-				<div className="max-w-screen-md flex flex-row justify-evenly mx-auto px-2 md:px-0">
-					<div className="grow text-center max-w-[150px]">
-						<p className="my-0">Current Block</p>
-						<p className="my-0">{currentBlock.toLocaleString()}</p>
-					</div>
-					<div className="grow text-center max-w-[150px]">
-						<p className="my-0">Current Epoch</p>
-						<p className="my-0">{currentEpoch.toLocaleString()} <small className="text-[0.7rem]">{currentEpochIndex.toLocaleString()}/{currentEpochLength.toLocaleString()}</small></p>
-					</div>
-					<div className="grow text-center max-w-[150px]">
-						<p className="my-0">Target Epoch</p>
-						<p className="my-0">{targetEpoch.toLocaleString()}</p>
-					</div>
+				<div className="max-w-screen-sm flex flex-wrap flex-row justify-evenly mx-auto px-2 md:px-0 mb-8">
+					{renderStatsBox("Current Block", currentBlock.toLocaleString(), null)}
+					{renderStatsBox("Current Epoch", currentEpoch.toLocaleString(), `${currentEpochIndex.toLocaleString()}/${currentEpochLength.toLocaleString()}`)}
+					{renderStatsBox("Target Epoch", targetEpoch.toLocaleString(), null)}
 				</div>
 			</section>
-			<section className="max-w-screen-md min-w-[375px] mx-auto mb-24 px-2 md:px-0">
-				<article className="mb-16">
+			<section className="max-w-screen-sm mx-auto px-2 md:px-0 mb-12 h-64 md:h-72 overflow-visible" style={{background: 'url("nervos-logo-glowing.png") 50% 50%/contain no-repeat'}}>
+			</section>
+			<section className="max-w-screen-sm mx-auto mb-24 px-2 md:px-0">
+				<article className="mb-24">
 					<h2 className="text-center">What is a Halving Event?</h2>
 					<p>
-						In the Nervos ecosystem, mining is used to created and distribute new tokens within the ecosystem. A total of 33.6 billion CKB tokens will be created through primary issuance as block rewards to incentivize miners to secure the network.
+						In the Nervos ecosystem, mining is used to secure the network and distribute tokens in the form of block rewards. A total of 33.6 billion CKB tokens will be created through primary issuance over a period of approximately 84 years to incentivize the miners that secure the network.
 					</p>
 					<p>
-						Every epoch, a period of approximately four hours, a fixed amount of 1,917,808 CKB is introduced. Every 8,760 epochs, a period of approximately four years, the amount is cut in half. This event is called a <b>halving</b> and it is the point where the mining rewards from primary issuance are permanently reduced by 50%. This halving process will continue every four years until the year 2103, after which point all block rewards from primary issuance will cease completely.
+						Every epoch, a period of approximately four hours, a fixed amount of 1,917,808 CKB is introduced. Every 8,760 epochs, a period of approximately four years, this amount is cut in half. This event is called a halving and it is the point where the mining rewards from primary issuance are permanently reduced by 50%. This halving process will continue every four years until the year 2103, after which point all block rewards from primary issuance will cease completely.
 					</p>
-					<p>
-						<a href="nervos-halving-table.png" target="_blank" rel="noreferrer"><img className="max-w-xs mx-auto" src="nervos-halving-table.png" alt="Nervos Halving Table" /></a>
+					<p className="pt-8 text-center">
+						<a className="md:opacity-50 transition-opacity duration-500 hover:opacity-100" href="https://docs.nervos.org/docs/reference/halving/" target="_blank" rel="noreferrer"><img className="w-full max-w-lg inline" src="nervos-halving-table.png" alt="Nervos Halving Table" /></a>
 					</p>
 				</article>
-				<article className="mb-16">
+				<article className="mb-24">
 					<h2 className="text-center">What is the Significance of a Halving?</h2>
 					<p>
-						Each time a halving occurs, it causes a sharp decrease in the rewards generated per block. The supply of new CKB entering circulation is lowered, dramatically reducing the rate of inflation. This creates a shift in the underly market equilibrium and forces a reevaluation of what is considered fair market value.
+						Each time a halving occurs, it causes a sharp decrease in the rewards generated per block. The supply of new CKB entering circulation is lowered, dramatically reducing the rate of inflation. This is important because it creates a shift in the underlying market equilibrium and forces a reevaluation of what is considered fair market value.
 					</p>
 					<p>
-						<a href="https://explorer.nervos.org/charts/inflation-rate" target="_blank" rel="noreferrer"><img className="max-w-xs mx-auto" src="nervos-inflation-rate-chart.png" alt="Nervos CKB Inflation Rate Chart" /></a>
+						Halving events occur on a predetermined issuance schedule that cannot be changed, postponed, or delayed. Investors and community members often look forward to a halving event as something to celebrate since it marks an important milestone in the history of the project.
+					</p>
+					<p className="pt-8 text-center">
+						<a className="md:opacity-50 transition-opacity duration-500 hover:opacity-100" href="https://explorer.nervos.org/charts/inflation-rate" target="_blank" rel="noreferrer"><img className="w-full max-w-lg inline" src="nervos-inflation-rate-chart.png" alt="Nervos CKB Inflation Rate Chart" /></a>
 					</p>
 				</article>
-				<article className="mb-16">
+				<article className="mb-24">
 					<h2 className="text-center">How Could a Halving Affect the Price of CKB?</h2>
 					<p>
 						A general rule of thumb in economic theory states that steady demand with decreasing supply results in higher pricing. Bitcoin enthusiasts often use the stock-to-flow model as a way to measure the total amount of existing Bitcoin (stock) relative to the amount of Bitcoin produced per year (flow).
@@ -228,8 +277,8 @@ function App()
 					<p>
 						This stock-to-flow model predicts that the higher the stock-to-flow ratio, the higher the price of the asset. This model remains controversial among experts, especially for gauging short-term market sentiment. However, one look at Bitcoin's stock-to-flow chart leads many to believe the theory is worth further consideration.
 					</p>
-					<p>
-						<a href="https://www.lookintobitcoin.com/charts/stock-to-flow-model/" target="_blank" rel="noreferrer"><img className="max-w-xs mx-auto" src="bitcoin-stock-to-flow-chart.png" alt="Bitcoin Stock to Flow Chart" /></a>
+					<p className="pt-8 text-center">
+						<a className="md:opacity-50 transition-opacity duration-500 hover:opacity-100" href="https://www.lookintobitcoin.com/charts/stock-to-flow-model/" target="_blank" rel="noreferrer"><img className="w-full max-w-lg inline" src="bitcoin-stock-to-flow-chart.png" alt="Bitcoin Stock to Flow Chart" /></a>
 					</p>
 				</article>
 			</section>
